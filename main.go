8fbc42c92060f6bd7a0cf5d3f56f20e9aabf80d3
Bjoern Rabenstein <bjoern@soundcloud.com>
2014-06-20 11:45:38 +0200

    Internal metrics migrated.

--- main.go
+++ main.go
@@ -37,59 +37,93 @@ var (
 	persistenceFile     = flag.String("persistence.file", "", "File to persist metrics. If empty, metrics are only kept in memory.")
 	persistenceInterval = flag.Duration("persistence.interval", 5*time.Minute, "The minimum interval at which to write out the persistence file.")
 
-	internalMetrics = []*struct {
-		name   string
-		help   string
-		eval   func(*runtime.MemStats) float64
-		metric prometheus.Metric
-	}{
+	im = internalMetrics{
 		{
-			name: "instance_goroutine_count",
-			help: "The number of goroutines that currently exist.",
-			eval: func(ms *runtime.MemStats) float64 {
-				return float64(runtime.NumGoroutine())
-			},
-			metric: prometheus.NewGauge(),
+			desc: prometheus.NewDesc(
+				"runtime_goroutines_count",
+				"Number of goroutines that currently exist.",
+				nil, nil,
+			),
+			eval:    func(ms *runtime.MemStats) float64 { return float64(runtime.NumGoroutine()) },
+			valType: prometheus.GaugeValue,
 			// Not a counter, despite the name... It can go up and down.
 		},
 		{
-			name:   "instance_allocated_bytes",
-			help:   "Bytes allocated and still in use.",
-			eval:   func(ms *runtime.MemStats) float64 { return float64(ms.Alloc) },
-			metric: prometheus.NewGauge(),
+			desc: prometheus.NewDesc(
+				"runtime_memory_allocated_bytes",
+				"Bytes allocated and still in use.",
+				nil, nil,
+			),
+			eval:    func(ms *runtime.MemStats) float64 { return float64(ms.Alloc) },
+			valType: prometheus.GaugeValue,
 		},
 		{
-			name:   "instance_total_allocated_bytes",
-			help:   "Bytes allocated (even if freed).",
-			eval:   func(ms *runtime.MemStats) float64 { return float64(ms.TotalAlloc) },
-			metric: prometheus.NewGauge(),
+			desc: prometheus.NewDesc(
+				"runtime_memory_allocated_bytes_total",
+				"Total bytes ever allocated (even if freed).",
+				nil, nil,
+			),
+			eval:    func(ms *runtime.MemStats) float64 { return float64(ms.TotalAlloc) },
+			valType: prometheus.CounterValue,
 		},
 		{
-			name:   "instance_heap_allocated_bytes",
-			help:   "Heap bytes allocated and still in use.",
-			eval:   func(ms *runtime.MemStats) float64 { return float64(ms.HeapAlloc) },
-			metric: prometheus.NewGauge(),
+			desc: prometheus.NewDesc(
+				"runtime_memory_heap_allocated_bytes",
+				"Heap bytes allocated and still in use.",
+				nil, nil,
+			),
+			eval:    func(ms *runtime.MemStats) float64 { return float64(ms.HeapAlloc) },
+			valType: prometheus.GaugeValue,
 		},
 		{
-			name:   "instance_gc_high_watermark_bytes",
-			help:   "Next run in HeapAlloc time (bytes).",
-			eval:   func(ms *runtime.MemStats) float64 { return float64(ms.NextGC) },
-			metric: prometheus.NewGauge(),
+			desc: prometheus.NewDesc(
+				"runtime_gc_high_watermark_bytes",
+				"Next run in HeapAlloc time (bytes).",
+				nil, nil,
+			),
+			eval:    func(ms *runtime.MemStats) float64 { return float64(ms.NextGC) },
+			valType: prometheus.GaugeValue,
 		},
 		{
-			name:   "instance_gc_total_pause_ns",
-			help:   "Total GC pause time.",
-			eval:   func(ms *runtime.MemStats) float64 { return float64(ms.PauseTotalNs) },
-			metric: prometheus.NewGauge(),
+			desc: prometheus.NewDesc(
+				"runtime_gc_pause_ns",
+				"Total GC pause time.",
+				nil, nil,
+			),
+			eval:    func(ms *runtime.MemStats) float64 { return float64(ms.PauseTotalNs) },
+			valType: prometheus.GaugeValue,
 		},
 		{
-			name:   "instance_gc_count",
-			help:   "GC count.",
-			eval:   func(ms *runtime.MemStats) float64 { return float64(ms.NumGC) },
-			metric: prometheus.NewCounter(),
+			desc: prometheus.NewDesc(
+				"runtime_gc_total",
+				"Total number of GC runs.",
+				nil, nil,
+			),
+			eval:    func(ms *runtime.MemStats) float64 { return float64(ms.NumGC) },
+			valType: prometheus.CounterValue,
 		},
 	}
 )
+
+type internalMetrics []struct {
+	desc    *prometheus.Desc
+	eval    func(*runtime.MemStats) float64
+	valType prometheus.ValueType
+}
+
+func (im internalMetrics) Describe(ch chan<- *prometheus.Desc) {
+	for _, i := range im {
+		ch <- i.desc
+	}
+}
+
+func (im internalMetrics) Collect(ch chan<- prometheus.Metric) {
+	memStats := &runtime.MemStats{}
+	runtime.ReadMemStats(memStats)
+	for _, i := range im {
+		ch <- prometheus.MustNewConstMetric(i.desc, i.valType, i.eval(memStats))
+	}
+}
 
 func main() {
 	flag.Parse()
@@ -98,24 +132,16 @@ func main() {
 	flag.VisitAll(func(f *flag.Flag) {
 		flags[f.Name] = f.Value.String()
 	})
-	m := martini.Classic()
 
 	ms := storage.NewDiskMetricStore(*persistenceFile, *persistenceInterval)
-	prometheus.DefaultRegistry.SetMetricFamilyInjectionHook(ms.GetMetricFamilies)
+	prometheus.SetMetricFamilyInjectionHook(ms.GetMetricFamilies)
 
-	// The following demonstrate clearly the clunkiness of the current Go
-	// client library when it comes to values that are owned by other parts
-	// of the program and have to be evaluated on the fly. Let's leave it
-	// here as a demonstration and a benchmark for improvements of the
-	// library.
-	// TODO(bjoern): Update this once the new client library is out.
-	registerInternalMetrics()
-	m.Get("/metrics", http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			updateInternalMetrics()
-			prometheus.DefaultHandler(w, r)
-		}))
+	prometheus.MustRegister(im)
 
+	// TODO: Move from martini to github.com/julienschmidt/httprouter
+	// and then instrument all the handlers.
+	m := martini.Classic()
+	m.Get("/metrics", prometheus.Handler())
 	m.Put("/metrics/jobs/:job/instances/:instance", handler.Push(ms, true))
 	m.Post("/metrics/jobs/:job/instances/:instance", handler.Push(ms, false))
 	m.Delete("/metrics/jobs/:job/instances/:instance", handler.Delete(ms))
@@ -152,25 +178,4 @@ func interruptHandler(l net.Listener) {
 	<-notifier
 	log.Print("Received SIGINT/SIGTERM; exiting gracefully...")
 	l.Close()
-}
-
-func registerInternalMetrics() {
-	for _, im := range internalMetrics {
-		prometheus.Register(im.name, im.help, nil, im.metric)
-	}
-}
-
-func updateInternalMetrics() {
-	var memStats runtime.MemStats
-	runtime.ReadMemStats(&memStats)
-	for _, im := range internalMetrics {
-		switch m := im.metric.(type) {
-		case prometheus.Gauge:
-			m.Set(nil, im.eval(&memStats))
-		case prometheus.Counter:
-			m.Set(nil, im.eval(&memStats))
-		default:
-			log.Print("Unexpected metric type: ", m)
-		}
-	}
 }
