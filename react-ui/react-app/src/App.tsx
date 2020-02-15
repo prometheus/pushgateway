92028d868c40ff48751027c83f0e4a967146b857
Drumil Patel <drumilpatel720@gmail.com>
2020-02-15 12:44:54 +0530

    Create React app
    Signed-off-by: Drumil Patel <drumilpatel720@gmail.com>

---
+++ react-ui/react-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Push Gateway
+      </header>
+    </div>
+  );
+}
+
+export default App;
