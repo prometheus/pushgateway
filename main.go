86430ae549a6332638d89550b66237884902c5ab
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2023-10-07 12:39:24 -0500

    Optimize computeRoutePrefix
    Previously, computeRoutePrefix was adding a '/' to the start of the prefix and then removing it from the end, regardless of whether it was there or not. This resulted in unnecessary string operations. The updated function only adds or removes the '/' when necessary, improving efficiency. Additionally, the function now uses strings.HasPrefix and strings.HasSuffix for clearer intent and readability.
    
    Signed-off-by: donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>

--- main.go
+++ main.go
@@ -251,15 +251,22 @@ func computeRoutePrefix(prefix string, externalURL *url.URL) string {
 	}
 
 	if prefix == "/" {
-		prefix = ""
+		return ""
 	}
 
-	if prefix != "" {
-		prefix = "/" + strings.Trim(prefix, "/")
+	// Ensure prefix starts with "/"
+	if !strings.HasPrefix(prefix, "/") {
+		prefix = "/" + prefix
+	}
+
+	// Ensure prefix does not end with "/"
+	if strings.HasSuffix(prefix, "/") {
+		prefix = strings.TrimSuffix(prefix, "/")
 	}
 
 	return prefix
 }
+
 
 // shutdownServerOnQuit shutdowns the provided server upon closing the provided
 // quitCh or upon receiving a SIGINT or SIGTERM.
