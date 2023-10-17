e3409b1317791e819ab30c498a517d4f28799dd3
Julien Midedji <Julien.Midedji@gmail.com>
2023-10-17 18:42:36 +0200

    Keep record of unwinded containers on page refresh (#581)
    Keep record of unwinded containers on page refresh
    
    The solution retains a record of the currently un-collapsed container and unwinds it on a page refresh. No extra refresh button and no live-update.
    The minor inconvenience of this solution is that on a page refresh the unwind animation plays.
    
    Helps #568
    
    Signed-off-by: Julien Midedji <julien.midedji@gmail.com>
    
    ---------
    
    Signed-off-by: Julien Midedji <julien.midedji@gmail.com>

--- resources/static/functions.js
+++ resources/static/functions.js
@@ -119,11 +119,21 @@ $(function () {
 	    .removeClass('glyphicon-collapse-down')
 	    .addClass('glyphicon-collapse-up');
 	event.stopPropagation();
+
+	sessionStorage.setItem("coll_" + this.id, true);
     })
     $('div.collapse').on('hide.bs.collapse', function (event) {
 	$(this).prev().find('span.toggle-icon')
 	    .removeClass('glyphicon-collapse-up')
 	    .addClass('glyphicon-collapse-down');
 	event.stopPropagation();
+
+	sessionStorage.setItem("coll_" + this.id, false);
     })
+
+    $("div.collapse").each(function() {
+	if (sessionStorage.getItem("coll_" + this.id) == "true") {
+	    $(this).collapse("show");
+	}
+    });
 })
