44fa056f687ca85ad975950766da5f1a7b566d61
Vasilis Themelis <vdthemelis@gmail.com>
2024-06-06 17:57:21 +0100

    Log when liveness probes fails due to full write queue (#645)
    Log when liveness probes fails due to full write queue
    
    Signed-off-by: Vasilis Themelis <vdthemelis@gmail.com>
    Signed-off-by: Björn Rabenstein <github@rabenste.in>
    Co-authored-by: Björn Rabenstein <github@rabenste.in>

--- storage/diskmetricstore.go
+++ storage/diskmetricstore.go
@@ -125,7 +125,9 @@ func (dms *DiskMetricStore) Healthy() error {
 	// A pushgateway that cannot be written to should not be
 	// considered as healthy.
 	if len(dms.writeQueue) == cap(dms.writeQueue) {
-		return fmt.Errorf("write queue is full")
+		err := fmt.Errorf("write queue is full")
+		level.Warn(dms.logger).Log("msg", err)
+		return err
 	}
 
 	return nil
