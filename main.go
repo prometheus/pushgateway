f0da01fd4365126fb334aa8c251536840dd4db3a
atompi <atompi@foxmail.com>
2024-06-22 10:06:07 +0800

    feat: add --web.disable-exporter-metrics flag
    If this flag is set, the metrics about the pushgateway itself (go_*, process_*, promhttp_*) will be excluded from /metrics.
    
    This is useful in environments where you want to avoid recursive metric collection, i.e. you don't want the pushgateway's client_go default metrics to be exposed by itself.
    
    Signed-off-by: atompi <atompi@foxmail.com>

--- main.go
+++ main.go
@@ -63,17 +63,18 @@ func (lf logFunc) Println(v ...interface{}) {
 
 func main() {
 	var (
-		app                 = kingpin.New(filepath.Base(os.Args[0]), "The Pushgateway").UsageWriter(os.Stdout)
-		webConfig           = webflag.AddFlags(app, ":9091")
-		metricsPath         = app.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
-		externalURL         = app.Flag("web.external-url", "The URL under which the Pushgateway is externally reachable.").Default("").URL()
-		routePrefix         = app.Flag("web.route-prefix", "Prefix for the internal routes of web endpoints. Defaults to the path of --web.external-url.").Default("").String()
-		enableLifeCycle     = app.Flag("web.enable-lifecycle", "Enable shutdown via HTTP request.").Default("false").Bool()
-		enableAdminAPI      = app.Flag("web.enable-admin-api", "Enable API endpoints for admin control actions.").Default("false").Bool()
-		persistenceFile     = app.Flag("persistence.file", "File to persist metrics. If empty, metrics are only kept in memory.").Default("").String()
-		persistenceInterval = app.Flag("persistence.interval", "The minimum interval at which to write out the persistence file.").Default("5m").Duration()
-		pushUnchecked       = app.Flag("push.disable-consistency-check", "Do not check consistency of pushed metrics. DANGEROUS.").Default("false").Bool()
-		promlogConfig       = promlog.Config{}
+		app                    = kingpin.New(filepath.Base(os.Args[0]), "The Pushgateway").UsageWriter(os.Stdout)
+		webConfig              = webflag.AddFlags(app, ":9091")
+		metricsPath            = app.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
+		externalURL            = app.Flag("web.external-url", "The URL under which the Pushgateway is externally reachable.").Default("").URL()
+		routePrefix            = app.Flag("web.route-prefix", "Prefix for the internal routes of web endpoints. Defaults to the path of --web.external-url.").Default("").String()
+		enableLifeCycle        = app.Flag("web.enable-lifecycle", "Enable shutdown via HTTP request.").Default("false").Bool()
+		enableAdminAPI         = app.Flag("web.enable-admin-api", "Enable API endpoints for admin control actions.").Default("false").Bool()
+		disableExporterMetrics = app.Flag("web.disable-exporter-metrics", "Exclude metrics about the exporter itself (promhttp_*, process_*, go_*).").Default("false").Bool()
+		persistenceFile        = app.Flag("persistence.file", "File to persist metrics. If empty, metrics are only kept in memory.").Default("").String()
+		persistenceInterval    = app.Flag("persistence.interval", "The minimum interval at which to write out the persistence file.").Default("5m").Duration()
+		pushUnchecked          = app.Flag("push.disable-consistency-check", "Do not check consistency of pushed metrics. DANGEROUS.").Default("false").Bool()
+		promlogConfig          = promlog.Config{}
 	)
 	promlogflag.AddFlags(app, &promlogConfig)
 	app.Version(version.Print("pushgateway"))
@@ -100,11 +101,18 @@ func main() {
 		}
 	}
 
-	ms := storage.NewDiskMetricStore(*persistenceFile, *persistenceInterval, prometheus.DefaultGatherer, logger)
+	gatherer := prometheus.DefaultGatherer
+	if *disableExporterMetrics {
+		gr := prometheus.NewRegistry()
+		gr.MustRegister(versioncollector.NewCollector("pushgateway"))
+		gatherer = gr
+	}
 
-	// Create a Gatherer combining the DefaultGatherer and the metrics from the metric store.
+	ms := storage.NewDiskMetricStore(*persistenceFile, *persistenceInterval, gatherer, logger)
+
+	// Create a Gatherer combining the DefaultGatherer or empty and the metrics from the metric store.
 	g := prometheus.Gatherers{
-		prometheus.DefaultGatherer,
+		gatherer,
 		prometheus.GathererFunc(func() ([]*dto.MetricFamily, error) { return ms.GetMetricFamilies(), nil }),
 	}
 
